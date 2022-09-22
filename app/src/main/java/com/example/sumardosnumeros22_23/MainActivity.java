9759e6f6cfa2d957c9c725f320cf74c44b16d72c
manuel <mpgprofe@iesflorenciopintado.es>
2022-09-22 11:15:39 +0200

    Añado la resta y los logs y sobreescribo los método del Activity.

--- app/src/main/java/com/example/sumardosnumeros22_23/MainActivity.java
+++ app/src/main/java/com/example/sumardosnumeros22_23/MainActivity.java
@@ -3,17 +3,36 @@ package com.example.sumardosnumeros22_23;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText n1, n2;
-    Button bSumar;
+    Button bSumar, bRestar;
     TextView resultado;
+    final String ETIQUETA = "CALCULADORA";
 
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.i(ETIQUETA, "Entro en postResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ETIQUETA, "Se destruye la aplicación");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ETIQUETA, "Se mete en OnStop");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +45,57 @@ public class MainActivity extends AppCompatActivity {
         bSumar = findViewById(R.id.buttonSumar);
         resultado = findViewById(R.id.textViewResultado);
         resultado.setText("0");
+        bRestar = findViewById(R.id.buttonRestar);
 
+        /*
         bSumar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-/*
-                int num1 = Integer.parseInt(n1.getText().toString());
-                int num2 = Integer.parseInt(n2.getText().toString());
-                int suma = num1 + num2;
-                resultado.setText(String.valueOf(suma));
-*/
+
+               // int num1 = Integer.parseInt(n1.getText().toString());
+               // int num2 = Integer.parseInt(n2.getText().toString());
+                //int suma = num1 + num2;
+                //resultado.setText(String.valueOf(suma));
+
 
                resultado.setText(""+(Integer.parseInt(n1.getText().toString()) +Integer.parseInt(n2.getText().toString())));
+               Log.i(ETIQUETA, "He sumado dos números.");
+
+
             }
         });
 
+        bRestar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num1 = Integer.parseInt(n1.getText().toString());
+                int num2 = Integer.parseInt(n2.getText().toString());
+                int resta = num1 - num2;
+                resultado.setText(String.valueOf(resta));
+            }
+        });
+*/
+        bRestar.setOnClickListener(this);
+        bSumar.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int num1 = Integer.parseInt(n1.getText().toString());
+        int num2 = Integer.parseInt(n2.getText().toString());
+        int resultadoOperacion = 0;
+        switch (view.getId()) {
+            case R.id.buttonSumar:
+                resultadoOperacion = num1 + num2;
+                break;
+            case R.id.buttonRestar:
+                resultadoOperacion = num1 - num2;
+                break;
+
+        }
+        resultado.setText(""+resultadoOperacion);
 
     }
 }
